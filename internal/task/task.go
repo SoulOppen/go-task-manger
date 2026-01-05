ff2a45a0033c952803a699355fc884f80ffa6398
SoulOppen <arieloppenheimer@outlook.cl>
2026-01-05 11:38:08 -0300

    fix:Task basic

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,20 +1,24 @@
 package task
-import(
+
+import (
 	"fmt"
 )
+
 type Task struct {
 	ID      string  `json:"id"`
 	Proyect *string `json:"project,omitempty"`
 	Name    string  `json:"name"`
 }
-var agenda Task[]
-func addTask(task Task){
-	agenda= append(agenda,task)
+
+var agenda []Task
+
+func addTask(task Task) {
+	agenda = append(agenda, task)
 	for _, t := range agenda {
 		t.printTask()
 	}
 }
-func (t Task) printTask(){
+func (t Task) printTask() {
 	if t.Project != nil {
 		fmt.Printf("%s - %s - %s\n", t.ID, *t.Project, t.Name)
 	} else {
