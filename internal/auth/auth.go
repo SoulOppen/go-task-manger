6032055e36d8a8f7dcafd9a2ebb5732e2930bb50
SoulOppen <arieloppenheimer@outlook.cl>
2026-01-29 15:31:02 -0300

    add:crea archivo con usuarios

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func SignUp() {
@@ -12,10 +14,25 @@ func SignUp() {
 	fmt.Println("¿Cual es tu nombre de usuario?")
 	scanner.Scan()
 	username := scanner.Text()
-	_ = username
 	fmt.Println("¿Cual es tu clave?")
 	scanner.Scan()
 	password := scanner.Text()
 	_ = password
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		os.Exit(1)
+	}
+	dir := filepath.Join(confDir, "usuarios.txt")
+	data, err := os.ReadFile(dir)
+	if err != nil {
+		os.Exit(1)
+	}
+	contenido := strings.Split(string(data), "\n")
+	contenido = append(contenido, username)
+	texto := strings.Join(contenido, "\n")
+	err = os.WriteFile(dir, []byte(texto), 0644)
+	if err != nil {
+		os.Exit(1)
+	}
 	fmt.Println("Fuiste registrado con exito")
 }
